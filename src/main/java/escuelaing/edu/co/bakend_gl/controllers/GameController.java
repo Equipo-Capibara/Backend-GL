d0c1c52b5b7453dd60765aaab0a687a47a29577b
Joan-Acevedo <acevedojoan22@gmail.com>
2025-05-20 21:42:29 -0500

    Cambio en GameController

--- src/main/java/escuelaing/edu/co/bakend_gl/controllers/GameController.java
+++ src/main/java/escuelaing/edu/co/bakend_gl/controllers/GameController.java
@@ -1,24 +1,28 @@
 package escuelaing.edu.co.bakend_gl.controllers;
 
 import escuelaing.edu.co.bakend_gl.model.board.Board;
+import escuelaing.edu.co.bakend_gl.model.dto.BoardStateDto;
 import escuelaing.edu.co.bakend_gl.services.GameService;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType; // nuevo
+import org.springframework.http.ResponseEntity; // nuevo
 import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
 @RestController
-@RequestMapping("/game")
+@RequestMapping("/api/game")
 @RequiredArgsConstructor
 public class GameController {
 
     private final GameService gameService;
 
-    @GetMapping("/state")
-    @ResponseBody
-    public Board getGameState(@RequestParam String roomCode) {
-        return gameService.getBoard(roomCode);
+    @GetMapping("/{roomCode}/state")
+    public ResponseEntity<Board> getGameState(@PathVariable String roomCode) {
+        Board board = gameService.getBoard(roomCode);
+        return ResponseEntity.ok(board);
     }
 
     @GetMapping("/switch-level")
