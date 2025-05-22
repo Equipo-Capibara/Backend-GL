4c817124e9b6363174a795852ac0c9c36ef6a9dd
AndresSu2342 <cesar.borray-s@mail.escuelaing.edu.co>
2025-05-22 06:19:50 -0500

    Encryptacion Move Player Temporal

---
+++ src/main/java/escuelaing/edu/co/bakend_gl/util/AESUtil.java
@@ -0,0 +1,22 @@
+package escuelaing.edu.co.bakend_gl.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESUtil {
+
+    private static final String ALGORITHM = "AES";
+    private static final String SECRET_KEY = "1234567890123456"; // Debe tener 16 caracteres
+
+    public static String decrypt(String encryptedText) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+
+        return new String(decryptedBytes);
+    }
+}
